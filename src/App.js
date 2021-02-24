d5d289eb34cd83298a927a522c23db4c3954036f
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-23 21:31:27 -0800

    fixes for vercel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 
-import { Counter } from "./features/counter/Counter";
+//import { Counter } from "./features/counter/Counter";
 import "./App.css";
-import SignIn from "./components/SignIn";
-import Signup from "./components/Signup";
-import HomeProtectedPage from "./components/HomeProtectedPage";
-import NewRecipe from "./components/NewRecipe";
-import Recipe from "./components/Recipe";
-import EditRecipe from "./components/EditRecipe";
-import Nav from "./components/Nav";
 
 function App() {
   return (

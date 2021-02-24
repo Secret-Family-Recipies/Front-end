b50c51b8f5fb6ad39d932e1f87950382e1d02b1d
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-23 21:45:55 -0800

    start of add recipe

--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -4,6 +4,29 @@ const EditRecipe = () => {
   return (
     <div>
       <h1>EditRecipe</h1>
+      <form>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" />
+        <label htmlFor="Createdby">Created by:</label>
+        <input type="text" name="source" />
+        <label htmlFor="Ingredients">Ingredients</label>
+        <input type="text" name="Ingredients" />
+        <label htmlFor="instructions">Instructions</label>
+        <input type="text" name="instructions" />
+        <label htmlFor="category ">category </label>
+        <select type="category" name="category">
+          <option value="">Please Choose an option</option>
+          <option value="dessert">Dessert</option>
+          <option value="dinner">Dinner</option>
+          <option value="breakfast">Breakfast</option>
+          <option value="lunch">Lunch</option>
+          <option value="brunch">Brunch</option>
+          <option value="snack">Snack</option>
+          <option value="drink">Drink</option>
+        </select>
+        <button>Add Recipe</button>
+        <button>Cancel</button>
+      </form>
     </div>
   );
 };

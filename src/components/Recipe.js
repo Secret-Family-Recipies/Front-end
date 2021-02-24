8f895352044eafbadc5634aef1382d2ff2b63e1d
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-23 21:37:41 -0800

    base of recipie

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Recipes = () => {
   return (
-    <div>
-      <h1>Recipes</h1>
-    </div>
+    <>
+      <h1>Name of Recipe</h1>
+      <h3>Created by: ADD DATA</h3>
+      <h3>Ingredients: ADD DATA</h3>
+      <h3>Instructions: ADD DATA</h3>
+      <h3>Category: ADD DATA</h3>
+      <button>Edit</button>
+      <button>Delete</button>
+    </>
   );
 };
 

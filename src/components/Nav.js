7fa5728c4c7b195139bbfb518b4938df20a10f6b
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-23 22:08:44 -0800

    setup and start of routing completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
 import React from "react";
+import NavImg from "../assets/NavImg.png";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <div>
-      <h1>nav</h1>
+      <h1>Secret Family Recipes</h1>
+      <Link to="/signin">Logout</Link>
+      <Link to="/recipes">Recipes</Link>
+      <img src={NavImg} className="nav" alt="food" />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import HomeProtectedPage from "./components/HomeProtectedPage";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Route exact path="/signin" component={SignIn}></Route>
+        <Route exact path="/recipes" component={HomeProtectedPage}></Route>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")

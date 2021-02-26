a9c9b9688415fa8d4e9fa89579635c0391ee142b
INFOCORP\Casey.Dietz <caseydietz20@gmail.com>
2021-02-25 19:14:38 -0700

    Added JSX markup to component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
 const SignIn = () => {
+
+  const [formState, setFormState] = useState({
+    username: "",
+    password: ""
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+
+  //onChange function
+  const inputChange = e => {
+    
+  }
+
+
+  // sign in button function
+  const formSignIn = e => {
+    e.preventDefault()
+    console.log('Signed in!')
+    axios
+      .post("#", formState)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+  }
+
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <form onSubmit={formSignIn}>
+      <label htmlFor="username">
+        Username
+        <input
+          type="text"
+          name="username"
+          value={formState.name}
+          onChange={inputChange}
+          />
+      </label>
+
+      <label htmlFor="password">
+        Password
+        <input
+          type="text"
+          name="password"
+          value={formState.password}
+          onChange={inputChange}
+          />
+      </label>
+    </form>
   );
 };
 
